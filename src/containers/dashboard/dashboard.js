08f84b5334f20c5fb0e43cca31c7c5fd9d5d06d5
Kane Toomer <kanetoomer@yahoo.com>
2021-07-10 13:42:32 -0700

    Dashboard UI Complete

--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -1,12 +1,70 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView, FlatList } from 'react-native'
+
+// Vector Icons
+import Feather from "react-native-vector-icons/Feather";
+
 
 // Style Sheet
 import styles from './styles';
 
 const Dashboard = () => {
+
+    // Flatlist Dummy Data
+    const data = [
+        {
+            id: 0,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'June 01, 2021', 
+        },
+        {
+            id: 1,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'May 01, 2021', 
+        },
+        {
+            id: 2,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'April 01, 2021', 
+        },
+        {
+            id: 3,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'March 01, 2021', 
+        },
+        {
+            id: 4,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'February 01, 2021', 
+        },
+        {
+            id: 5,
+            title: 'Rent Payment',
+            amount: '1500',
+            date: 'January 01, 2021', 
+        },
+        {
+            id: 6,
+            title: 'Security Deposit',
+            amount: '250',
+            date: 'June 01, 2021', 
+        },
+    ]
+
+     // Separator
+    const renderSeparator = () => {
+        return (
+        <View style={{ height: 0.5, backgroundColor: "#CED0CE" }} />
+        );
+    };
+
     return (
-        <ScrollView style={styles.container}>
+        <View style={styles.container}>
             {/* Header */}
             <View style={styles.header}>
                 <Text style={styles.greeting}>Hi, Kane!</Text>
@@ -19,7 +77,33 @@ const Dashboard = () => {
                     <Text style={styles.buttonText}>Make a Payment</Text>
                 </TouchableOpacity>
             </View>
-        </ScrollView>
+            
+            {/* Transactions */}
+            <SafeAreaView style={styles.sectionSpacing}>
+                <Text style={styles.flatlistTitle}>Transaction History</Text>
+                <FlatList
+                    data={data}
+                    keyExtractor={item => item.address}
+                    renderItem={({ item }) => (
+                        <View style={styles.transactionContainer}>
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                                <View>
+                                    <Text style={styles.transactionTitle}>{item.title}</Text>
+                                    <View style={{ flexDirection: "row", marginTop: 10 }}>
+                                        <Feather name="clock" color="#34383D80" size={15} />
+                                        <Text style={styles.transactionText}>{item.date}</Text>
+                                    </View>
+                                </View>
+                                <Text style={styles.transactionAmount}>${item.amount}</Text>
+                            </View>
+                        </View>
+                    )}
+                    contentContainerStyle={{ paddingBottom: 10 }}
+                    showsVerticalScrollIndicator={false}
+                    ItemSeparatorComponent={renderSeparator}
+                />
+            </SafeAreaView>
+        </View>
     )
 }
 
--- src/containers/dashboard/styles.js
+++ src/containers/dashboard/styles.js
@@ -8,11 +8,12 @@ const styles = StyleSheet.create({
     },
     header: {
         backgroundColor: '#232256',
-        height: 500,
+        height: 400,
+        top: 0
     },
     greeting: {
         color: '#fff',
-        top: 100,
+        top: 80,
         left: 30,
         fontSize: 20,
         fontWeight: '600'
@@ -20,7 +21,7 @@ const styles = StyleSheet.create({
     amountDue: {
         color: '#fff',
         textAlign: 'center',
-        top: 150,
+        top: 130,
         fontSize: 35,
         fontWeight: '600'
     },
@@ -28,24 +29,72 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#ffffff90',
         textAlign: 'center',
-        top: 200,
+        top: 180,
     },
     paymentDate: {
         color: '#ffffff90',
         fontSize: 18,
         fontWeight: '500',
         textAlign: 'center',
-        top: 200,
+        top: 180,
         marginLeft: 7
     },
     button: {
         backgroundColor: '#fff',
         padding: 20,
         marginHorizontal: 30,
-        
+        borderRadius: 10,
+        top: 230
     },
     buttonText: {
-        color: '#232256'
+        color: '#232256',
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '600'
+    },
+    buttonContainer: {
+        marginHorizontal: 5,
+        marginTop: 10,
+        marginBottom: 20,
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    sectionSpacing: {
+        flex: 1
+    },
+    flatlistTitle: {
+        marginTop: 15,
+        marginLeft: 20,
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#34383D90'
+    },
+    transactionContainer: {
+        paddingVertical: 20,
+        paddingHorizontal: 25,
+    },
+    transactionTitle: {
+        color: '#34383D',
+        fontSize: 18,
+        fontWeight: '600',
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        marginTop: 10, 
+        alignItems: 'center',
+    },
+    transactionAmount: {
+        color: '#34383D',
+        fontSize: 16,
+        fontWeight: '600',
+        flexDirection: 'row', 
+    },
+    transactionText: {
+        paddingLeft: 12.5,
+        fontSize: 14,
+        fontWeight: '600',
+        color: '#34383D80',
+        alignSelf: 'center',
     },
 
     // MISC
